299a30203e00e1261ba91e1f852de8a120783a11
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 <galluccimia@gmail.com>
2022-10-02 09:48:06 -0400

    PyBank updates 10.2.22

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,30 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
+    first_row = next(csvreader)
+
+    for row in csvreader:
+        # total_months = row[0]
+        # total_net = row[1]
+        # net_change = total_net
+        # net_change_list = net_change
+
+        # total_months += 1
+        # total_net += int(first_row[1])
+        # prev_net = int(first_row[1])
+
+        # total_months += 1
+        # total_net += int(row[1])
+
+        # net_change = int(row[1]) - prev_net
+        # prev_net = int(row[1])
+        # net_change_list += [net_change]
+        
+
+        months = row[0]
+        profit_loss = row[1]
+        net_change = profit_loss
+
 # Calculate Total Months
     months = csvreader
     def month_year(month):
@@ -23,15 +47,26 @@ with open(csvpath) as csvfile:
     print("Total Months:" + str(var))
 
 #Calculate net total amount of "Profit/Losses" over the entire period
-    total = csvreader
-    def profit_loss(total):
-        profit_losses = 0
-        for i in total:
-            profit_losses +=1
 
-        return total
-    var1=profit_loss(total)
-    print("Total:" + str(var1))
+
+
+    # total = csvreader
+    # def profit_loss(total):
+    #     profit_losses = 0
+    #     for i in total:
+    #         profit_losses +=1
+
+    #     return profit_losses
+    # var1=profit_loss(total)
+    # print("Total:" + str(var1))
+
+#Average Change
+
+#Greatest Increase in Profits
+
+
+
+#Greatest Decrease in Profits
 
 #Write to .txt file
 with open(output_path, "w") as txtfile:
